b630d28ff6bdc3f3aa9f6dd09ce250aeb0910d9f
Jon <adityatomar7758@gmail.com>
2026-07-26 09:03:01 +0530

    Describe what you finished

--- services/loot.ts
+++ services/loot.ts
@@ -1,6 +1,8 @@
 import { lootTable } from "@/data/lootTable";
+import { shopItems } from "@/data/shopItems";
+import { LootItem } from "@/types/loot";
 
-export function rollLoot() {
+export function rollLoot(): LootItem {
   const roll = Math.random() * 100;
 
   let total = 0;
@@ -9,9 +11,33 @@ export function rollLoot() {
     total += loot.chance;
 
     if (roll <= total) {
-      return loot;
+      const item = shopItems.find(
+        (shopItem) => shopItem.id === loot.itemId
+      );
+
+      if (!item) {
+        break;
+      }
+
+      return {
+        id: item.id,
+        name: item.name,
+        icon: item.icon,
+        category: item.category,
+        rarity: loot.rarity,
+      };
     }
   }
 
-  return lootTable[0];
+  const item = shopItems.find(
+    (shopItem) => shopItem.id === lootTable[0].itemId
+  )!;
+
+  return {
+    id: item.id,
+    name: item.name,
+    icon: item.icon,
+    category: item.category,
+    rarity: lootTable[0].rarity,
+  };
 }
